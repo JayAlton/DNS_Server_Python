d7c341a4b247725968d86f4229b1674b8b20fe07
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-26 02:36:52 -0800

    Parse Header Section

--- app/main.py
+++ app/main.py
@@ -1,93 +1,9 @@
 import socket
-from dataclasses import dataclass
-import struct
-from typing import ClassVar
-
-@dataclass
-class DNSMessage:
-    id : int
-    qr : int
-    op_code: int
-    aa : int
-    tc : int
-    rd : int
-    ra: int
-    z : int
-    rcode : int
-    qdcount: int
-    ancount: int
-    nscount: int
-    arcount: int
-
-    ID_LEN: ClassVar[int] = 16
-    QR_LEN: ClassVar[int] = 1
-    OP_CODE_LEN: ClassVar[int] = 4
-    AA_LEN : ClassVar[int] = 1
-    TC_LEN : ClassVar[int] = 1
-    RD_LEN : ClassVar[int] = 1
-    RA_LEN : ClassVar[int] = 1
-    Z_LEN : ClassVar[int] = 3
-    RCODE_LEN: ClassVar[int] = 4
-    QDCOUNT_LEN : ClassVar[int] = 16
-    ANCOUNT_LEN : ClassVar[int] = 16
-    NSCOUNT_LEN : ClassVar[int] = 16
-    ARCOUNT_LEN : ClassVar[int] = 16
-
-
-    def serialize(self) -> str :
-        # assumes bit endian
-        combined1 = 0
-        cur_shift = 0
-        for val, delta_shift in zip(
-            (self.rd,self.tc, self.aa, self.op_code, self.qr),
-            (
-                DNSMessage.RD_LEN,
-                DNSMessage.TC_LEN,
-                DNSMessage.AA_LEN,
-                DNSMessage.OP_CODE_LEN,
-                0
-
-            )
-        ):
-            combined1 |= val << cur_shift
-            cur_shift += delta_shift
-
-        # assume big endian
-        combined2 = 0
-        cur_shift = 0
-        for val, delta_shift in zip(
-            (self.rcode, self.z, self.ra),
-            (DNSMessage.RCODE_LEN, DNSMessage.Z_LEN, DNSMessage.RA_LEN)
-        ):
-            combined2 |= val << cur_shift
-            cur_shift += delta_shift
-
-
-        return struct.pack(
-            ">HBBHHHH",
-            self.id,
-            combined1,
-            combined2,
-            self.qdcount,
-            self.ancount,
-            self.nscount,
-            self.arcount
-        )
-
-def encode_url(url):
-    response = b''
-    domains = url.split('.')
-    for domain in domains:
-        response += struct.pack('>B', len(domain))
-        response += b''.join((struct.pack('>B', ord(char)) for char in domain))
-
-    response += struct.pack('>B', 0)
-    return response 
-    
+from ipaddress import IPv4Address
+from .dns_message import ARecord, Message, Question, RecordType, RecordClass
 
 
 def main():
-    RDLENGTH = 4
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
@@ -96,47 +12,32 @@ def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
 
-    type_dict_python_int = {'A' : 1}
-    type_dict = {k : struct.pack('>H', v) for k, v in type_dict_python_int.items()}
-
-    class_dict_python_int = {"IN" : 1}
-    class_dict = {k : struct.pack('>H', v) for k, v in class_dict_python_int.items()}
-
-    first = True
+    print("Listening on port 2053...")
 
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
     
-            response = b""
-            if first:
-                response_data = DNSMessage(
-                    id = 1234,
-                    qr = 1,
-                    op_code = 0,
-                    aa = 0,
-                    tc = 0,
-                    rd = 0,
-                    ra = 0,
-                    z = 0,
-                    rcode = 0,
-                    qdcount= 1,
-                    ancount= 1,
-                    nscount= 0,
-                    arcount= 0
-                )
-            response = response_data.serialize()
+            id = int.from_bytes(buf[:2], "big")
+            replyMessage = Message.build_reply(
+                id = id,
+                questions=[
+                    Question(
+                        name="codecrafters.io",
+                        type=RecordType.A,
+                        klass=RecordClass.IN,
+                    )
+                ],
+                resource_records=[
+                    ARecord("codecrafters.io", 60, IPv4Address("192.168.1.1"))
+                ],
+            )
 
-            # added the question section 
-
-            question = encode_url('codecrafters.io') + type_dict['A'] + class_dict['IN']
-            response += question
-
-            answer = encode_url('codecrafters.io') + type_dict['A'] + class_dict['IN'] + struct.pack('>I', 60) + struct.pack('>H', RDLENGTH) + struct.pack('>BBBB', 8,8,8,8)
-            response += answer
-
+            
+            print(reply_message.to_bytes())
+            print(len(reply_message.to_bytes()))
     
-            udp_socket.sendto(response, source)
+            udp_socket.sendto(reply_message.to_bytes(), source)
         except Exception as e:
             print(f"Error receiving data: {e}")
             break
