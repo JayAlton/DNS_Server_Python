5751e5faadf968a436acf55844dd5b3c0ce4032c
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-26 01:48:05 -0800

    Writing Question Section

--- app/main.py
+++ app/main.py
@@ -1,5 +1,24 @@
 import socket
 
+import struct
+from dataclasses import dataclass
+
+@dataclass
+class DNSMessage:
+    id: int
+    qr: int
+    opcode: int
+    aa: int
+    tc: int
+    rd: int
+    ra: int
+    z: int
+    rcode: int
+    qdcount: int
+    ancount: int
+    nscount: int
+    arcount: int
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,14 +32,53 @@ def main():
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-    
-            response = b"\x04\xd2\x80" + (b"\x00" * 9)
+            question_section = buf[12:]
+            header = create_header(1)
+            response = pack_dns_message(header)
+            response += question_section
     
             udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")
             break
 
+def create_header(qdcount=0):
+    return DNSMessage(
+        id=1234,
+        qr=1,
+        opcode=0,
+        aa=0,
+        tc=0,
+        rd=0,
+        ra=0,
+        z=0,
+        rcode=0,
+        qdcount=qdcount,
+        ancount=0,
+        nscount=0,
+        arcount=0,
+    )
+
+def pack_dns_message(message: DNSMessage) -> bytes: 
+    flags = (
+        (message.qr << 15)
+        | (message.opcode << 11)
+        | (message.aa << 10)
+        | (message.tc << 9)
+        | (message.rd << 8)
+        | (message.ra << 7)
+        | (message.z << 4)
+        | message.rcode
+    )
+    return struct.pack(
+        ">HHHHHH",
+        message.id,
+        flags,
+        message.qdcount,
+        message.ancount,
+        message.nscount,
+        message.arcount
+    )
 
 if __name__ == "__main__":
     main()
